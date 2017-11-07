27fbabe809cb17b4e56041427620c15a0b091c61
Harry Rein <hrein@edx.org>
2017-11-07 17:06:55 -0500

    Extending functionality.

--- lms/static/js/learner_dashboard/views/course_entitlement_view.js
+++ lms/static/js/learner_dashboard/views/course_entitlement_view.js
@@ -21,10 +21,23 @@
              return Backbone.View.extend({
                  tpl: HtmlUtils.template(pageTpl),
 
+                 events: {
+                     'change .session-select': 'updateEnrollBtn',
+                     'click .enroll-btn': 'enrollInSession',
+                 },
+
                  initialize: function(options) {
                      this.$el = options.$el;
                      this.entitlementModel = new EntitlementModel();
+
+                     // Grab external dynamic elements and bind events
+                     this.$triggerOpenBtn = $(options.$triggerOpenBtn);
+                     this.$triggerOpenBtn.on('click', this.openPanel);
+
                      this.render(options);
+
+                     this.$sessionSelect = this.$el.find('.session-select');
+                     this.$enrollBtn = this.$el.find('.enroll-btn');
                  },
 
                  render: function(options) {
@@ -33,7 +46,22 @@
                          entitlementUUID: options.entitlementUUID
                      });
                      HtmlUtils.setHtml(this.$el, this.tpl(data));
-                 }
+                 },
+
+                 openPanel: function(e) {
+                     this.$el.removeClass('hidden');
+                 },
+
+                 enrollInSession: function(e) {
+                     alert("we want to enroll the user in course with id: " + $this.$sessionSelect.val());
+                     console.log(this.$el.find('option:selected').data('course_id'));
+                 },
+
+                 updateEnrollBtn: function() {
+                     var new_id = this.$el.find('option:selected').data('course_id');
+                     console.log("new_id");
+                     this.$enrollBtn.removeClass('disabled');
+                 },
              });
          }
     );
